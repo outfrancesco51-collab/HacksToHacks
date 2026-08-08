59e22b1533034fa436103baf7edc4ecb88b93e01
outfrancesco51-collab <outfrancesco51@gmail.com>
2026-08-08 21:01:14 +0200

    Add new CCTV and PC hack levels, generate Nano Banana Pro assets (censored), and fix missing audio files

--- src/main.c
+++ src/main.c
@@ -47,7 +47,6 @@ void LoadLanguage(const char* langCode) {
         UnloadFileText(data);
     }
     
-    // Ricarica audio
     if(IsAudioDeviceReady()) {
         UnloadSound(sndKarenIntro);
         UnloadSound(sndKarenError);
@@ -58,17 +57,20 @@ void LoadLanguage(const char* langCode) {
     }
 }
 
-// Global Vars
+// Variabili globali
 char playerName[MAX_INPUT_CHARS + 1] = "\0";
 int letterCount = 0;
 Texture2D texUI;
-Texture2D texFace1;
-Texture2D texFace2;
 Texture2D texNode;
 Font customFont;
 NPatchInfo uiPatch;
 
-// Finestre UI
+// Asset Bersagli
+Texture2D texBank;
+Texture2D texCCTV;
+Texture2D texPC;
+
+// UI
 Rectangle windowMap = { 50, 50, 800, 500 };
 bool draggingMap = false;
 Vector2 dragOffset = {0, 0};
@@ -77,34 +79,46 @@ Vector2 dragOffset = {0, 0};
 float titleAnimTime = 0.0f;
 float settingsScrollY = 0.0f;
 float typewriterTime = 0.0f;
-int menuSelection = 0; // 0=Play, 1=Options, 2=Exit
+int menuSelection = 0; 
+int selectedTarget = 0; // 0=Bank, 1=CCTV, 2=PC
 
 // Hacker Typer
 char typedCode[MAX_FAKE_CODE] = {0};
 int typedLen = 0;
-const char* fakeSource = 
-    "#include <sys/socket.h>\n"
-    "int main() {\n"
-    "    struct sockaddr_in server_addr;\n"
-    "    int sockfd = socket(AF_INET, SOCK_STREAM, 0);\n"
-    "    if (sockfd < 0) return -1;\n"
-    "    server_addr.sin_family = AF_INET;\n"
-    "    server_addr.sin_port = htons(8080);\n"
-    "    inet_pton(AF_INET, \"192.168.1.1\", &server_addr.sin_addr);\n"
-    "    connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));\n"
-    "    send(sockfd, \"PAYLOAD_INJECT\", 14, 0);\n"
-    "    // BYPASSING FIREWALL...\n"
-    "    uint32_t mem_offset = 0x004050A0;\n"
-    "    inject_dll(mem_offset);\n"
-    "    return 0;\n"
-    "}\n\n"
-    "function rootkit_deploy() {\n"
-    "    sys_override(true);\n"
-    "    disable_logs();\n"
-    "}\n";
 bool hackGranted = false;
 float grantedTimer = 0.0f;
 bool errorPlayed = false;
+
+const char* fakeSourceBank = 
+    "#include <sys/socket.h>\n"
+    "int main() {\n"
+    "    struct sockaddr_in bank_server;\n"
+    "    int sockfd = socket(AF_INET, SOCK_STREAM, 0);\n"
+    "    connect(sockfd, (struct sockaddr*)&bank_server, sizeof(bank_server));\n"
+    "    send(sockfd, \"OVERRIDE_AUTH_FUNDS\", 19, 0);\n"
+    "    // BYPASSING FIREWALL...\n"
+    "    inject_dll(0x004050A0);\n"
+    "    return 0;\n"
+    "}\n";
+
+const char* fakeSourceCCTV = 
+    "#!/bin/bash\n"
+    "echo 'Intercepting CCTV feed...'\n"
+    "airmon-ng start wlan0\n"
+    "airodump-ng -c 6 --bssid 00:14:6C:7E:40:80 wlan0mon\n"
+    "aireplay-ng -0 5 -a 00:14:6C:7E:40:80 wlan0mon\n"
+    "ffmpeg -i rtsp://192.168.1.10:554/feed -vcodec copy out.mp4\n"
+    "echo 'Feed hijacked.'\n";
+
+const char* fakeSourcePC = 
+    "import os, requests\n"
+    "def extract_files(target_ip):\n"
+    "    print('Connecting to backdoor...')\n"
+    "    r = requests.get(f'http://{target_ip}:8080/exploit')\n"
+    "    if r.status_code == 200:\n"
+    "        print('Decrypting personal folders...')\n"
+    "        os.system('tar -xzf /mnt/C/Users/Victim/Desktop/Secret.tar.gz')\n"
+    "extract_files('192.168.1.101')\n";
 
 void DrawTextHacker(const char *text, float x, float y, float fontSize, Color color) {
     DrawTextEx(customFont, text, (Vector2){x, y}, fontSize, 2, color);
@@ -118,9 +132,10 @@ int main(void)
     InitAudioDevice();
 
     texUI = LoadTexture("assets/ui_1.jpg");
-    texFace1 = LoadTexture("assets/face1_1.jpg");
-    texFace2 = LoadTexture("assets/face2_1.jpg"); // Il banchiere
     texNode = LoadTexture("assets/node_1.jpg");
+    texBank = LoadTexture("assets/face2_1.jpg"); 
+    texCCTV = LoadTexture("assets/target_cctv.jpg");
+    texPC = LoadTexture("assets/target_pc.jpg");
     customFont = LoadFontEx("assets/font.ttf", 64, 0, 0);
     
     LoadLanguage("it");
@@ -147,8 +162,9 @@ int main(void)
     UnloadSound(sndKarenSuccess);
     UnloadFont(customFont);
     UnloadTexture(texUI);
-    UnloadTexture(texFace1);
-    UnloadTexture(texFace2);
+    UnloadTexture(texBank);
+    UnloadTexture(texCCTV);
+    UnloadTexture(texPC);
     UnloadTexture(texNode);
     if(localeData) cJSON_Delete(localeData);
     
@@ -199,7 +215,6 @@ void UpdateDrawFrame(void)
                 } else if (menuSelection == 1) {
                     currentScreen = SETTINGS;
                 } else if (menuSelection == 2) {
-                    // Quit game
 #if !defined(PLATFORM_WEB)
                     CloseWindow();
                     exit(0);
@@ -209,6 +224,9 @@ void UpdateDrawFrame(void)
             break;
             
         case GAMEPLAY:
+            if (IsKeyPressed(KEY_DOWN)) selectedTarget = (selectedTarget + 1) % 3;
+            if (IsKeyPressed(KEY_UP)) selectedTarget = (selectedTarget - 1 + 3) % 3;
+            
             if (IsKeyPressed(KEY_H)) {
                 currentScreen = HACKING_MINIGAME;
                 typedLen = 0;
@@ -237,31 +255,31 @@ void UpdateDrawFrame(void)
             
             if (!hackGranted) {
                 int key = GetCharPressed();
+                const char* src = (selectedTarget == 0) ? fakeSourceBank : (selectedTarget == 1 ? fakeSourceCCTV : fakeSourcePC);
+                
                 if (key > 0) {
-                    // Aggiungi un pezzo di codice finto
                     int charsToAdd = 5; 
                     for(int i=0; i<charsToAdd; i++) {
-                        if (typedLen < strlen(fakeSource) && typedLen < MAX_FAKE_CODE-1) {
-                            typedCode[typedLen] = fakeSource[typedLen];
+                        if (typedLen < strlen(src) && typedLen < MAX_FAKE_CODE-1) {
+                            typedCode[typedLen] = src[typedLen];
                             typedLen++;
                         }
                     }
                     
-                    if (typedLen >= strlen(fakeSource) - 10) {
+                    if (typedLen >= strlen(src) - 5) {
                         hackGranted = true;
                         grantedTimer = 0.0f;
                         PlaySound(sndKarenSuccess);
                     }
                 }
                 
-                // Se non digita niente e preme backspace, si incazza
                 if (IsKeyPressed(KEY_BACKSPACE) && !errorPlayed) {
                     PlaySound(sndKarenError);
                     errorPlayed = true;
                 }
             } else {
                 grantedTimer += dt;
-                if (grantedTimer > 3.0f) {
+                if (grantedTimer > 4.0f) {
                     currentScreen = GAMEPLAY;
                 }
             }
@@ -272,7 +290,7 @@ void UpdateDrawFrame(void)
             if (settingsScrollY < -500.0f) settingsScrollY = SCREEN_HEIGHT;
             if (IsKeyPressed(KEY_ESCAPE)) currentScreen = TITLE;
             
-            if (IsKeyPressed(KEY_ONE) || IsKeyPressed(KEY_TWO) || IsKeyPressed(KEY_THREE)) {
+            if (IsKeyPressed(KEY_ONE) || IsKeyPressed(KEY_TWO)) {
                 if (IsKeyPressed(KEY_ONE)) LoadLanguage("it");
                 if (IsKeyPressed(KEY_TWO)) LoadLanguage("en");
             }
@@ -294,7 +312,6 @@ void UpdateDrawFrame(void)
             float yPos = TweenValue(-100.0f, 150.0f, titleAnimTime, TWEEN_EASE_OUT_BOUNCE);
             DrawTextHacker(GetText("TITLE"), SCREEN_WIDTH/2 - MeasureTextEx(customFont, GetText("TITLE"), 100, 2).x/2, yPos, 100, HACKER_GREEN);
             
-            // Menu Classico
             int menuY = 400;
             const char* mPlay = GetText("MENU_PLAY");
             const char* mOpt = GetText("MENU_OPTIONS");
@@ -304,19 +321,16 @@ void UpdateDrawFrame(void)
             DrawTextHacker(mOpt, SCREEN_WIDTH/2 - MeasureTextEx(customFont, mOpt, 40, 2).x/2, menuY + 60, 40, menuSelection == 1 ? WHITE : HACKER_DARK_GREEN);
             DrawTextHacker(mExit, SCREEN_WIDTH/2 - MeasureTextEx(customFont, mExit, 40, 2).x/2, menuY + 120, 40, menuSelection == 2 ? WHITE : HACKER_DARK_GREEN);
             
-            // Tutorial SYS-KAREN
             if(titleAnimTime > 1.0f) {
                 Rectangle karenBox = {20, 20, 400, 250};
                 DrawRectangle(karenBox.x + 10, karenBox.y + 10, karenBox.width, karenBox.height, (Color){0,0,0,150});
                 DrawRectangleRec(karenBox, DARK_BG);
                 DrawTextureNPatch(texUI, uiPatch, karenBox, (Vector2){0,0}, 0.0f, WHITE);
-                
                 DrawTextHacker("SYS-KAREN", karenBox.x + 20, karenBox.y + 15, 30, RED);
                 
                 const char* fullText = GetText("TUTORIAL_SYSKAREN_1");
-                int totalChars = strlen(fullText);
                 int displayChars = (int)typewriterTime;
-                if(displayChars > totalChars) displayChars = totalChars;
+                if(displayChars > strlen(fullText)) displayChars = strlen(fullText);
                 
                 char displayStr[512] = {0};
                 strncpy(displayStr, fullText, displayChars);
@@ -327,19 +341,22 @@ void UpdateDrawFrame(void)
         case GAMEPLAY:
             DrawTextHacker("HACKS TO HACKS - VIRTUAL OS", 10, 10, 20, GRAY);
             DrawTextHacker(TextFormat("USER: %s", playerName), 10, 30, 20, HACKER_GREEN);
+            DrawTextHacker("FRECCE SU/GIU: CAMBIA BERSAGLIO | H: HACKERA", 10, 50, 20, WHITE);
             
-            // Finestra Mappa
             DrawRectangle(windowMap.x + 10, windowMap.y + 10, windowMap.width, windowMap.height, (Color){0,0,0,200});
             DrawRectangleRec(windowMap, DARK_BG);
             DrawTextureNPatch(texUI, uiPatch, windowMap, (Vector2){0,0}, 0.0f, WHITE);
-            
             DrawRectangle(windowMap.x+2, windowMap.y+2, windowMap.width-4, 40, HACKER_GREEN);
             DrawTextHacker(GetText("MAP_TITLE"), windowMap.x + 20, windowMap.y + 5, 30, BLACK);
             
-            DrawTextHacker(GetText("PRESS_H_BANK"), windowMap.x + 30, windowMap.y + 60, 30, HACKER_GREEN);
-            DrawTextHacker("TARGET: BANCA DI NISIDA", windowMap.x + 30, windowMap.y + 100, 24, WHITE);
+            const char* targetName = "";
+            Texture2D tTex;
+            if (selectedTarget == 0) { targetName = GetText("TARGET_BANK"); tTex = texBank; }
+            if (selectedTarget == 1) { targetName = GetText("TARGET_CCTV"); tTex = texCCTV; }
+            if (selectedTarget == 2) { targetName = GetText("TARGET_PC"); tTex = texPC; }
             
-            DrawTexture(texFace2, windowMap.x + 400, windowMap.y + 150, WHITE); // Usa banchiere
+            DrawTextHacker(TextFormat("TARGET: %s", targetName), windowMap.x + 30, windowMap.y + 80, 40, WHITE);
+            DrawTextureEx(tTex, (Vector2){windowMap.x + 300, windowMap.y + 150}, 0.0f, 0.4f, WHITE); // Scala per farle stare nella finestra
             DrawTexture(texNode, windowMap.x + 50, windowMap.y + 150, WHITE);
             
             if (((int)(GetTime() * 4)) % 2 == 0) {
@@ -348,16 +365,18 @@ void UpdateDrawFrame(void)
             break;
             
         case HACKING_MINIGAME:
-            // Hacker Typer
             DrawTextHacker(">>> TERMINALE DI INJECTION <<<", 20, 20, 40, HACKER_GREEN);
             DrawTextHacker("PREMI TASTI A CASO PER GENERARE IL PAYLOAD...", 20, 60, 20, GRAY);
             
-            DrawTextEx(customFont, typedCode, (Vector2){20, 100}, 20, 2, HACKER_GREEN);
+            DrawTextEx(customFont, typedCode, (Vector2){20, 100}, 24, 2, HACKER_GREEN);
             
             if (hackGranted) {
+                Texture2D resTex = (selectedTarget == 0) ? texBank : (selectedTarget == 1 ? texCCTV : texPC);
+                DrawTextureEx(resTex, (Vector2){SCREEN_WIDTH/2 - (resTex.width*0.5f)/2, 100}, 0.0f, 0.5f, WHITE); // Mostra l'asset estratto
+                
                 if (((int)(GetTime() * 8)) % 2 == 0) {
-                    DrawRectangle(SCREEN_WIDTH/2 - 250, SCREEN_HEIGHT/2 - 50, 500, 100, HACKER_GREEN);
-                    DrawTextHacker(GetText("ACCESS_GRANTED"), SCREEN_WIDTH/2 - 190, SCREEN_HEIGHT/2 - 30, 60, BLACK);
+                    DrawRectangle(SCREEN_WIDTH/2 - 250, SCREEN_HEIGHT/2 + 50, 500, 100, HACKER_GREEN);
+                    DrawTextHacker(GetText("ACCESS_GRANTED"), SCREEN_WIDTH/2 - 190, SCREEN_HEIGHT/2 + 70, 60, BLACK);
                 }
             }
             break;
@@ -369,7 +388,6 @@ void UpdateDrawFrame(void)
             break;
     }
     
-    // Scanlines
     for(int i = 0; i < SCREEN_HEIGHT; i+=3) {
         DrawLine(0, i, SCREEN_WIDTH, i, (Color){0, 10, 0, 70});
     }
